c860f4ebba2491e0ffaad9b381dc83ce095e4971
Lucas Araujo <lucascdearaujo444@gmail.com>
2023-01-20 21:42:52 -0300

    deletando diretório inutilizado e corrigindo a seção sobre

--- src/components/AboutContainer/index.jsx
+++ src/components/AboutContainer/index.jsx
@@ -3,12 +3,13 @@ function AboutContainer() {
     <section className="about-container">
       <h2>Sobre</h2>
       <p className="about-text">
-        Olá! <br /> Me chamo Lucas Carneiro de Araujo, tenho 21 anos e estou
-        começando minha carreira na área de desenvolvimento web, mais
-        especificamente em front end. <br /> Busco me atualizar e melhorar
-        minhas habilidades dia a dia, gosto de enfrentar desafios e testar
-        minhas capacidades, com a finalidade de aprender com o processo. Possuo
-        boa comunicação, energia e relacionamento interpessoal
+        Olá! <br /> Me chamo Lucas Carneiro de Araújo, 
+        tenho 21 anos e estou começando minha carreira dentro da área de desenvolvimento web, 
+        com foco em Front End. <br />
+        Venho estudando há 8 meses e sinto a cada projeto concluído que a profissão de desenvolvedor 
+        é onde posso demonstrar todo meu potencial e paixão pela área.
+        Ainda estou em fase de expansão, buscando cada vez mais me aprimorar e aprender novas 
+        tecnologias e metodologias. Sempre gostei de desafios e estou aberto a novas experiências.
       </p>
     </section>
   );
