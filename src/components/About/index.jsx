5b6bf3da589dcdfa2a38a66f600bb826904010db
Lucas Araujo <lucascdearaujo444@gmail.com>
2023-02-08 18:25:05 -0300

    alterando imagem de perfil de acordo com o tamanho do dispositivo

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,27 +1,53 @@
 import { Box, Typography, Stack } from "@mui/material"
 import BigCleanButton from "../../../public/assets/buttons/BigButton/BigCleanButton"
 import { SimpleButton } from "../../../public/assets/buttons/SimpleButton/SimpleButton"
-import { useEffect } from "react"
+import { useEffect, useState} from "react"
 import AOS from 'aos'
 
 function About() {
+  const [isDesktop, setIsDesktop] = useState(true)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [window.innerWidth])
 
   useEffect(() => {
     AOS.init()
   }, [])
+
+  const handleResize = () => {
+    if(window.innerWidth > 821) {
+      setIsDesktop(true)
+    } else {
+      setIsDesktop(false)
+    }
+  }
 
   return (
     <Box mt={8} display='flex' gap={{md: '20%'}} sx={{
         justifyContent: 'space-between',
         width: '100%',
         maxWidth: '1600px',
-        height: 'auto'
+        height: 'auto',
+        flexWrap: {sm: 'wrap', xs: 'wrap', md: 'nowrap'},
     }}>
-        <Box display='flex' flexDirection='column' width='60%' height='auto'>
+        <Box
+          display='flex'
+          flexDirection='column'
+          height='auto'
+          sx={{
+            width: {md: '60%', sm: '100%'},
+            marginBottom: {sm: '40px', md: '0px'}
+          }}
+        >
             <Typography variant="h3"
                 sx={{fontFamily: 'Playfair Display',
-                width: '100%',
-                fontSize: {md: '52px', sm: '33px'},
+                width: '98%',
+                fontSize: {md: '52px', sm: '50px'},
                 fontWeight: '400',
                 }}
 
@@ -34,12 +60,12 @@ function About() {
             <Typography
                 sx={{
                     fontFamily: 'poppins',
-                    opacity: '0.7',
-                    color: '#828686',
-                    width: {md: '95%', sm: '85%'},
-                    mt: '2rem',
-                    fontSize: {md: '20px', sm: '18px'},
-                    lineHeight: '26px'
+                    opacity: '0.5',
+                    color: '#808080',
+                    width: {md: '95%', sm: '95%'},
+                    mt: '37px',
+                    fontSize: {md: '20px', sm: '24px'},
+                    lineHeight: {md: '26px', sm: '35px'}
                 }}
 
                 data-aos="fade-right"
@@ -56,8 +82,17 @@ function About() {
                 </Stack>
             </Box>
         </Box>
-       <Box height={{md: 'auto'}} width={{md: '60%', sm: '50%'}}>
-            <img data-aos="flip-right" className="main-image" data-aos-duration="2000" src="./assets/images/Lucas Araujo.jpeg" alt="foto de perfil" />
+       <Box
+          height={{md: 'auto'}}
+          width={{md: '60%', sm: '98%'}}
+        >
+            {
+              isDesktop
+              ?
+              (<img data-aos="flip-right" className="main-image" data-aos-duration="2000" src="./assets/images/Lucas Araujo.jpeg" alt="foto de perfil" />)
+              :
+              (<img data-aos="flip-right" className="main-image" data-aos-duration="2000" src="./assets/images/LucasAraujo02.png" alt="foto de perfil" />)
+            }
        </Box>
     </Box>
   )
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -3,36 +3,36 @@ export const projectsList = [
     id: 'dashboard',
     name: 'Dashboard',
     link: 'https://professional-dashboard-app.netlify.app/',
-    thumbnail: './assets/images/thumbnails/Dashboard.PNG'
+    thumbnail: '../../public/assets/images/thumbnails/Dashboard.PNG'
   },
   {
     id: 'cryptoNews',
     name: 'CryptoNews',
     link: 'https://cryptoappnews.netlify.app/',
-    thumbnail: './assets/images/thumbnails/Cryptonews.PNG'
+    thumbnail: '../../public/assets/images/thumbnails/Cryptonews.PNG'
   },
   {
     id: 'ourtube',
     name: 'OurTube',
     link: 'https://ourtube01.netlify.app/',
-    thumbnail: './assets/images/thumbnails/OurTube.PNG'
+    thumbnail: '../../public/assets/images/thumbnails/OurTube.PNG'
   },
   {
     id: 'forca',
     name: 'Forca',
     link: 'https://lucasa0.github.io/Jogo-da-Forca/index.html',
-    thumbnail: './assets/images/thumbnails/Forca.PNG'
+    thumbnail: '../../public/assets/images/thumbnails/Forca.PNG'
   },
   {
     id: 'pomodoro',
     name: 'Pomodoro',
     link: 'https://pomodoro-app-76ab.netlify.app/',
-    thumbnail: './assets/images/thumbnails/Pomodoro.PNG'
+    thumbnail: '../../public/assets/images/thumbnails/Pomodoro.PNG'
   },
   {
     id: 'netflixClone',
     name: 'NetflixClone',
     link: 'https://cloned-netflix-app.netlify.app/',
-    thumbnail: './assets/images/thumbnails/NetflixClone.PNG'
+    thumbnail: '../../public/assets/images/thumbnails/NetflixClone.PNG'
   },
 ]
