cfc42726a7f4394efda7a9aeb9aae3cef71fab59
Lucas Araujo <lucascdearaujo444@gmail.com>
2023-02-10 17:42:12 -0300

    corrigindo importações antes do merge

--- src/utils/socials.js
+++ src/utils/socials.js
@@ -2,21 +2,21 @@ export const socialsList = [
   {
     id: 'whatsapp',
     name: 'Whatsapp',
-    thumbnail: './assets/icons/socials/WhatsApp_white.png',
+    thumbnail: '/assets/icons/socials/WhatsApp_white.png',
     link: 'https://api.whatsapp.com/send/?phone=21993586894&text&type=phone_number&app_absent=0',
     target: '(21) 99358-6894'
   },
   {
     id: 'linkedin',
     name: 'Linkedin',
-    thumbnail: './assets/icons/socials/LinkedIN_white.png',
+    thumbnail: '/assets/icons/socials/LinkedIN_white.png',
     link: 'https://www.linkedin.com/in/lucas-c-araujo/',
     target: 'lucas-c-araujo'
   },
   {
     id: 'instagram',
     name: 'Instagram',
-    thumbnail: './assets/icons/socials/Instagram_white.png',
+    thumbnail: '/assets/icons/socials/Instagram_white.png',
     link: 'https://www.instagram.com/lucas_c_araujo12/',
     target: 'lucas_c_araujo12'
   },
@@ -24,13 +24,13 @@ export const socialsList = [
     id: 'github',
     name: 'Github',
     link: 'https://github.com/LucasA0',
-    thumbnail: './assets/icons/socials/Github_white.png',
+    thumbnail: '/assets/icons/socials/Github_white.png',
     target: 'LucasA0'
   },
   {
     id: 'discord',
     name: 'Discord',
-    thumbnail: './assets/icons/socials/Discord_white.png',
+    thumbnail: '/assets/icons/socials/Discord_white.png',
     target: 'Lucas.#7549'
   },
 ]
